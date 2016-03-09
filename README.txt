dfc61f25cf13a1c58126587ca8acddc330e66813
Austin Parsons <parsons_aj@comcast.net>
2016-03-09 14:29:37 -0500

    Power Function fully functioning.

---
+++ README.txt
@@ -0,0 +1,5 @@
+Austin Parsons
+
+This program takes a base and an exponent and gives you the answer.
+
+
---
+++ powerFunction.py
@@ -0,0 +1,22 @@
+
+
+def power(base, exponent): 
+	''' Takes a base and puts it to the power of the input exponant
+	and then returns the product'''
+	
+	tally = 0
+	answer = 1
+	while tally < exponent:
+		answer = base * answer
+		tally += 1
+	
+	return answer
+
+--------------MAIN PROGRAM--------------
+
+base = int(input("What will the base be?"))
+exponent = int(input("What will the exponent be?"))
+
+answer = power(base, exponent) 
+
+print("{} to the power of {} is {}".format(base,exponent,answer)
